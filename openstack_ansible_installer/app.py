1379b73edb51016b6f82cb38effec91429192068
Dmitriy Rabotyagov <dmitriy@adria-cloud.com>
2025-09-28 11:34:03 +0200

    Add serving TUI as a web app
    It could be handy to run the TUI in a web browser
    in some cases. While we do not add any auth or security implementation,
    app can be served behind a proxy with auth to
    provide some level of security.
    
    Signed-off-by: Dmitriy Rabotyagov <dmitriy@adria-cloud.com>

--- openstack_ansible_installer/app.py
+++ openstack_ansible_installer/app.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 
 from textual.app import App
 
@@ -44,6 +45,63 @@ class OpenStackAnsibleApp(App):
         self.push_screen(InitialCheckScreen())
 
 
-def main():
+def serve_app(args):
+    try:
+        from textual_serve.server import Server
+    except ImportError as err:
+        raise SystemExit(
+            'Required dependencies are missing to serve as a web application! '
+            'Please, make sure that OpenStack-Ansible Wizard is installed with '
+            '`serve` extras.\n'
+            'Try running: `pip install openstack-ansible-wizard[serve]`'
+        )
+
+    server = Server(
+        command=args.prog, title="OpenStack-Ansible Wizard",
+        host=args.host, port=args.port, public_url=args.url
+    )
+    server.serve()
+
+
+def run_app(args):
     app = OpenStackAnsibleApp()
     app.run()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Startup arguments for OpenStack-Ansible Wizard. "
+                    "Launch without arguments for the console TUI",
+        prog="openstack-ansible-installer",
+    )
+    parser.set_defaults(func=run_app)
+
+    subparsers = parser.add_subparsers(dest='subparser_command', help='subcommand help')
+    serve_parser = subparsers.add_parser(
+        "serve",
+        help="Run OpenStack-Ansible Wizard as a web application"
+    )
+    serve_parser.add_argument(
+        "-H", "--host",
+        help="IP address or FQDN to run the application on",
+        default="localhost"
+    )
+    serve_parser.add_argument(
+        "-p", "--port",
+        help="Port to run the application on",
+        default=8080, type=int
+    )
+    serve_parser.add_argument(
+        "-u", "--url",
+        help="The public URL, if the server is behind a proxy",
+    )
+    serve_parser.set_defaults(func=serve_app)
+
+    return parser
+
+
+def main():
+    parser = parse_args()
+    args = parser.parse_args()
+    args.prog = parser.prog
+    args.func(args)
